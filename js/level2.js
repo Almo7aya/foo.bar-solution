32b1f69eee755d0c163a9e3ec97ecb8efbbe8199
Ali Almohaya <AliAlmohaya@gmail.com>
2018-05-14 20:57:14 +0300

    add level2 js

---
+++ js/level2.js
@@ -0,0 +1,18 @@
+let i, j;
+let rs = '';
+for (i = 2; i < 30000; i++) {
+    for (j = 2; j <= i; j++) {
+        if (i == j) {
+            rs = rs + i;
+        }
+        if (i % j == 0) {
+            break;
+        }
+    }
+}
+
+function answer(n) {
+    return rs.split('').slice(n, n + 5).join('');
+}
+
+console.log(answer(0));
