7bc10888ae87cb20903a11ea38895e8815ed0013
Ali Almohaya <AliAlmohaya@gmail.com>
2018-05-14 19:02:21 +0300

    add python code

---
+++ py/level1.py
@@ -0,0 +1,26 @@
+def answer(n):
+    arr = []
+    minSum = n
+    while minSum:
+        last = getMaxSq(minSum)
+        minSum -= last
+        arr.append(last)
+    
+    return arr
+
+def getMaxSq(n):
+    i = 1
+    lastSq = 0
+    while 1:
+        square = i ** 2
+        if square >= n:
+            break
+        lastSq = square;
+        i += 1
+
+    if lastSq == 0:
+        lastSq = 1
+
+    return lastSq
+
+print( answer(12) )
